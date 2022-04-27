474e9cb2ead0f0c625cbfc872f0e53c571c1ee7b
js.lozano@globant.com <142308Jslp>
2022-04-26 21:29:57 -0500

    Added driver handler methods beforeTest and afterTest in BaseTest class

--- src/test/java/com/automation/web/tests/LoginTest.java
+++ src/test/java/com/automation/web/tests/LoginTest.java
@@ -1,4 +1,29 @@
 package com.automation.web.tests;
 
+import driver.Driver;
+import org.apache.log4j.Logger;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import pages.HomePage;
+
 public class LoginTest {
+    Driver driver;
+
+    private HomePage homePage;
+    public Logger log = Logger.getLogger(LoginTest.class);
+
+    @BeforeTest
+    @Parameters({"browser", "url"})
+    public void beforeTest(){
+        driver = new Driver("chrome");
+        driver.getDriver().manage().window().maximize();
+        driver.getDriver().get("https://www.espnqa.com/?src=com&_adblock=true&espn=cloud");
+        homePage = new HomePage(driver.getDriver());
+    }
+
+    @AfterTest
+    public void afterTest(){
+        driver.getDriver().quit();
+    }
 }
